b2b09bcf9276647eca2e89493f24806da2c344fe
minh-atrihub <minhhn@atrihub.io>
2021-05-04 12:22:51 -0700

    feat(training): TDD Tutorial
    Chapter 5: Saving User Input
    Complete
    
    [ICTRAINING-666]

--- functional_tests.py
+++ functional_tests.py
@@ -13,12 +13,8 @@ class NewVisitorTest(unittest.TestCase):
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_element_by_tag_name('tr')
-        if len(rows) > 1:
-            self.assertIn(row_text, [row.text for row in rows])
-        else:
-            self.assertIn(row_text, rows.text)
-
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -36,7 +32,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
-        input_box.send_keys('1: Buy peacock feathers')
+        input_box.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
@@ -48,7 +44,7 @@ class NewVisitorTest(unittest.TestCase):
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         input_box = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
-        input_box.send_keys('2: Use peacock feathers to make a fly')
+        input_box.send_keys('Use peacock feathers to make a fly')
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
