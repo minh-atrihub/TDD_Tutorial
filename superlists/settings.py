3617b1346751f1dad99277a9ba9902ac15406d4d
minh-atrihub <minhhn@atrihub.io>
2021-05-05 18:18:53 -0700

    feat(training): TDD Tutorial
    Chapter 8: Styling
    Complete chapter 8 and Heroku deploy
    
    [ICTRAINING-669]

--- superlists/settings.py
+++ superlists/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+if 'HEROKU_APP' in os.environ:
+    # Activate Django-Heroku
+    import django_heroku
+    django_heroku.settings(locals())
