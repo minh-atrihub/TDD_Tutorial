c654c07d01d0c127f8b7d23b72e7e672caeac754
minh-atrihub <minhhn@atrihub.io>
2021-05-05 18:12:16 -0700

    feat(training): TDD Tutorial
    Chapter 8: Styling
    Complete chapter 8 and Heroku deploy
    
    [ICTRAINING-669]

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,13 @@
 from django.test import LiveServerTestCase
 import time
 from selenium import webdriver
-import unittest
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+
 
 MAX_WAIT = 10
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -110,6 +111,23 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        # inputbox = self.browser.find_element_by_id('id_new_item')
+        # self.assertAlmostEqual(
+        #     inputbox.location['x'] + inputbox.size['width'] / 2,
+        #     512,
+        #     delta = 20
+        # )
 
 # if __name__ == '__main__':
 #     unittest.main()
